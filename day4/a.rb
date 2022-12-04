51225a1d58f2cd46f00da46450fef4acdf13b0b5
Brian Piña <brian.pina@gmail.com>
2022-12-04 10:58:12 -0800

    day4 a

---
+++ day4/a.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+def overlapping?(pair)
+  left, right = pair.map { |x| (x[0]..x[1]) }
+
+  left.cover?(right) || right.cover?(left)
+end
+
+def number_of_overlaps(input, overlapping_ranges = 0)
+  return overlapping_ranges if input.empty?
+
+  current, *rest = input
+  pair = current.split(',').map {|x| x.split('-').map(&:to_i) }
+  overlapping_ranges += 1 if overlapping?(pair)
+
+  number_of_overlaps(rest, overlapping_ranges)
+end
+
+input = File.readlines('a.txt')
+p "found #{number_of_overlaps(input)} overlapping"
